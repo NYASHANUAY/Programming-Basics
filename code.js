9ce4a700d68abd965a65d8165fadd45c26aac0f0
Nyasha Nyemba <nyemban1974@gmail.com>
2023-07-09 21:42:39 -0400

    initial commit

---
+++ code.js
@@ -0,0 +1,19 @@
+
+
+function greet() {
+    function greeting(){
+        for (var t = 0 t<1930 t+=){
+         if (t>1930);
+         return "Good day Anakin";
+        t = 2000;
+}
+   else;
+
+} 
+return "noGood night Anakin"
+var word = greeting();
+console.log(word);
+}
+greet()
+
+
